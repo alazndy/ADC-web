2ec9963156590f065955f7c50b7cdc53f76e8c15
Göktuğ Turhan <goktugturhan74@gmail.com>
2025-09-25 11:06:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7919284954-7ec23",
+  "appId": "1:291270213013:web:d1e8470702268d45698a9c",
+  "apiKey": "AIzaSyAfAqVHRQ07B2xQt6DiHR0aBhh2HSlWbSk",
+  "authDomain": "studio-7919284954-7ec23.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "291270213013"
+};
